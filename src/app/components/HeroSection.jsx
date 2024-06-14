8690cc8732e5e67d91ed823d05dcb76ed27bf29a
Mehdi Raza <mehdiraza019mr@gmail.com>
2024-06-14 15:15:22 +0530

    vercel solve

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -63,8 +63,8 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image-2.png"
-              alt="hero image"
+              src="/images/hero-image-5.png"
+              alt=""
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
               height={400}
