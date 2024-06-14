449d2e81fd849d12bfc99e9b9d7de3b5caba19ef
Mehdi Raza <mehdiraza019mr@gmail.com>
2024-06-14 15:15:59 +0530

    vercel solve

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-const resend = new Resend("re_8XqRPXzN_7g545kZ7qkchVtDRLfTAK4Ma");
+// const resend = new Resend("re_8XqRPXzN_7g545kZ7qkchVtDRLfTAK4Ma");
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-  try {
-    const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "mehdiraza019mr@gmail.com",
-      subject: "Hello World",
-      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-    });
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+// export async function POST(req, res) {
+//   const { email, subject, message } = await req.json();
+//   console.log(email, subject, message);
+//   try {
+//     const data = await resend.emails.send({
+//       from: "onboarding@resend.dev",
+//       to: "mehdiraza019mr@gmail.com",
+//       subject: "Hello World",
+//       html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+//     });
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
