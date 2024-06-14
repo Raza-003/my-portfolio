248c0b600d1870882100a6693fb2403018081e3b
Mehdi Raza <mehdiraza019mr@gmail.com>
2024-06-14 14:57:03 +0530

    vercel

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,25 +1,17 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const resend = new Resend("re_8XqRPXzN_7g545kZ7qkchVtDRLfTAK4Ma");
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
+      from: "onboarding@resend.dev",
+      to: "mehdiraza019mr@gmail.com",
+      subject: "Hello World",
+      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
     });
     return NextResponse.json(data);
   } catch (error) {
