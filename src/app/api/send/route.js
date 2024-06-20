4f4a33e06d54893e848e5fa1f58d9dfb483d079c
Mehdi Raza <mehdiraza019mr@gmail.com>
2024-06-20 19:13:29 +0530

    vercel solve

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,23 +1,23 @@
-import { Resend } from 'resend';
-import EmailTemplate from '../../components/email-templete';
+// import { Resend } from 'resend';
+// import EmailTemplate from '../../components/email-templete';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Raza <mehdiraza019mr@gmail.com> ',
-      to: ['mehdiraza019mr@gmail.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({firstName: 'Raza'}),
-    });
+// export default async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'Raza <mehdiraza019mr@gmail.com> ',
+//       to: ['mehdiraza019mr@gmail.com'],
+//       subject: 'Hello world',
+//       react: EmailTemplate({firstName: 'Raza'}),
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
